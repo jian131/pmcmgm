2c1c5d050c3894015edf41d4c4561efd509e8f08
jian131 <dawndawn335@gmail.com>
2024-11-04 18:24:38 +0700

    Initial commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run()
+    {
+        Role::create(['name' => 'quản lý']);
+        Role::create(['name' => 'nhân viên']);
+    }
+}
